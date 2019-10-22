d8a8b5179bc44334419752156a61b553e0899b06
pilena24 <lenkazivkovic24@gmail.com>
2019-10-22 12:07:40 +0200

    Dodavanje atributa 'title' u linkove

--- views/fixed/header.php
+++ views/fixed/header.php
@@ -3,7 +3,7 @@
 					<div class="container">
 				  		<div class="row justify-content-center">
 						      <div id="logo">
-						        <a href="index.php"><img src="img/logo/logo_mala_bela.png" alt="logo" title="" /></a>
+						        <a title='Republika Fud' href="index.php"><img src="img/logo/logo_mala_bela.png" alt="logo" title="" /></a>
 						      </div>
 				  		</div>			  					
 					</div>
@@ -12,11 +12,11 @@
 					<div class="row align-items-center justify-content-center d-flex">			
 				      <nav id="nav-menu-container">
 				        <ul class="nav-menu">
-				          <li><a href="index.php">Početna</a></li>
-				          <li><a href="about.php">O nama</a></li>
-				          <li><a href="menu.php">Online naručivanje</a></li>
-				          <li><a href="gallery.php">Galerija</a></li>				          					          		          
-				          <li><a href="contact.php">Kontakt</a></li>
+				          <li><a title='Početna stranica' href="index.php">Početna</a></li>
+				          <li><a title='O nama' href="about.php">O nama</a></li>
+				          <li><a title='Online naručivanje' href="menu.php">Online naručivanje</a></li>
+				          <li><a title='Galerija' href="gallery.php">Galerija</a></li>				          					          		          
+				          <li><a title='Kontakt' href="contact.php">Kontakt</a></li>
 				        </ul>
 				      </nav><!-- #nav-menu-container -->					      		  
 					</div>
